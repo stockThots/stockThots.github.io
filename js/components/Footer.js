0d2dc7ae522b053271cb1f210e45f4ba43abb171
stockThots <stockthots27@gmail.com>
2025-02-21 22:04:43 -0700

    Create Footer.js

---
+++ js/components/Footer.js
@@ -0,0 +1,10 @@
+// js/components/Footer.js
+class Footer {
+    render() {
+      return `
+        <footer class="footer">
+          <p>&copy; ${new Date().getFullYear()} stockThots &copy; ${new Date().getFullYear()} A Ryan Ronish Production</p>
+        </footer>
+      `;
+    }
+  }
